eac68f785076dfb64813c1862c26abeaf33048fa
Sung Jinwoo <codes_of_war@Baki.>
2025-06-10 15:17:38 +0200

    health bar displayed

--- bonus/player/display_stats.c
+++ bonus/player/display_stats.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:31:43 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/06/10 15:15:20 by marvin           ###   ########.fr       */
+/*   Updated: 2025/06/10 15:17:33 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ void	init_hp_bar(t_stats *p_stats, t_mlx mlx)
 	p_stats->start_h_hp = WIN_HEIGHT / 20;
 	p_stats->start_w_hp = WIN_WIDTH / 20;
 	p_stats->hp_bar_w = (WIN_WIDTH / 4) - p_stats->start_w_hp;
-	p_stats->hp_bar_h = (WIN_HEIGHT / 20 * 1.5) - p_stats->start_h_hp;
+	p_stats->hp_bar_h = p_stats->start_h_hp / 2;
 	p_stats->hp_bar = mlx_new_image(mlx.init, p_stats->hp_bar_w,
 			p_stats->hp_bar_h);
 }
