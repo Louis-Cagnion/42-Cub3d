db373d7b29803a6c5b62b06dd940087674b49d03
Sung Jinwoo <codes_of_war@Baki.>
2025-06-10 16:02:23 +0200

    opti : pixels in a string and actualise only if hp change

--- bonus/player/display_stats.c
+++ bonus/player/display_stats.c
@@ -6,15 +6,16 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:31:43 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/06/10 15:59:43 by marvin           ###   ########.fr       */
+/*   Updated: 2025/06/10 16:02:01 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-static inline void	print_hp_bar(t_img *hp_bar, int *pixels_hp_bar, int hp_bar_h, int hp_bar_w)
+static inline void	print_hp_bar(t_img *hp_bar, int *pixels_hp_bar,
+	int hp_bar_h, int hp_bar_w)
 {
-	int i;
+	int	i;
 	int	width;
 	int	height;
 
@@ -33,12 +34,12 @@ static inline void	print_hp_bar(t_img *hp_bar, int *pixels_hp_bar, int hp_bar_h,
 	}
 }
 
-static inline void	put_pixels_hp_bar(int hps_left, t_stats *p_stats, int width, int height)
+static inline void	put_pixels_hp_bar(int hps_left, t_stats *p_stats, int width,
+	int height)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	
 	while (++width < p_stats->hp_bar_w)
 		p_stats->pixels_hp_bar[i++] = 0;
 	width = 1;
@@ -72,7 +73,8 @@ void	display_stats(t_stats p_stats, t_mlx mlx)
 		hps_left /= 2;
 		put_pixels_hp_bar(hps_left, &p_stats, 0, 1);
 	}
-	print_hp_bar(p_stats.hp_bar, p_stats.pixels_hp_bar, p_stats.hp_bar_h, p_stats.hp_bar_w);
+	print_hp_bar(p_stats.hp_bar, p_stats.pixels_hp_bar,
+		p_stats.hp_bar_h, p_stats.hp_bar_w);
 	mlx_put_image_to_window(mlx.init, mlx.window, p_stats.hp_bar,
 		p_stats.start_w_hp, p_stats.start_h_hp);
 }
@@ -85,6 +87,7 @@ void	init_hp_bar(t_stats *p_stats, t_mlx mlx)
 	p_stats->hp_bar_h = p_stats->start_h_hp / 2;
 	p_stats->hp_bar = mlx_new_image(mlx.init, p_stats->hp_bar_w,
 			p_stats->hp_bar_h);
-	p_stats->pixels_hp_bar = malloc(sizeof(int) * (p_stats->hp_bar_h * p_stats->hp_bar_w));
+	p_stats->pixels_hp_bar = malloc(sizeof(int)
+			* (p_stats->hp_bar_h * p_stats->hp_bar_w));
 	put_pixels_hp_bar(P_HPS, p_stats, -1, 1);
 }
