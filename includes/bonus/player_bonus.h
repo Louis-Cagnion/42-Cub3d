ccccb00f7eb9f258656f802b3dbbabaf5142d1bf
Sung Jinwoo <codes_of_war@Baki.>
2025-07-04 23:01:10 +0200

    weapon wheel structure done

--- includes/bonus/player_bonus.h
+++ includes/bonus/player_bonus.h
@@ -6,12 +6,29 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 19:01:51 by locagnio          #+#    #+#             */
-/*   Updated: 2025/06/10 12:08:51 by marvin           ###   ########.fr       */
+/*   Updated: 2025/07/04 23:01:04 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PLAYER_BONUS_H
 # define PLAYER_BONUS_H
+
+# include "bonus/entities_bonus.h"
+
+typedef struct s_weapon
+{
+	t_entity	default_sprite;
+	t_entity	*shot_animation;
+	t_entity	*reload_animation;
+	int			atk_points;
+}	t_weapon;
+
+typedef struct s_weapon_wheel
+{
+	t_weapon	handgun;
+	t_weapon	shotgun;
+	t_weapon	minigun;
+}	t_weapon_wheel;
 
 typedef struct s_player
 {
